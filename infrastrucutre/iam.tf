211dfcfc8af200b371bb500e3ca47c27a699869f
Stefan Aradjanin <stefan.aradjanin@vegait.rs>
2024-05-28 11:09:49 +0200

    Created custom role, oidc provider and ecr policy

---
+++ infrastrucutre/iam.tf
@@ -0,0 +1,56 @@
+
+module "iam_github_oidc_provider" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+}
+
+module "github_actions_ecr_policy" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = "VegaCourseTerraformGitHubActionPolicy"
+  path        = "/"
+  description = "Policy for GitHub Actions to interact with ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:DescribeRepositories",
+          "ecr:GetRepositoryPolicy",
+          "ecr:ListImages",
+          "ecr:DeleteRepository",
+          "ecr:BatchDeleteImage",
+          "ecr:SetRepositoryPolicy",
+          "ecr:DeleteRepositoryPolicy"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+module "iam_iam-assumable-role-with-oidc" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+
+  create_role = true
+
+  role_name        = "VegaCourseTerraformGitHubActionRole"
+  role_description = "GitHub Action Interaction Role"
+
+  role_policy_arns           = [module.github_actions_ecr_policy.arn]
+  number_of_role_policy_arns = 1
+
+  provider_url = module.iam_github_oidc_provider.url
+
+  #security-token-service
+  oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
+  oidc_subjects_with_wildcards   = ["repo:stefan-aradjanin/docker-nodejs-sample:*"]
+}
