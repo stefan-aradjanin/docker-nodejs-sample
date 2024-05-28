21504e3a451c6e0423d18d6ad3962e0b711e7463
Stefan Aradjanin <stefan.aradjanin@vegait.rs>
2024-05-28 10:25:33 +0200

    Merged subnet and vpc into network and added data source for az

--- infrastrucutre/network.tf
+++ infrastrucutre/network.tf
@@ -1,10 +1,58 @@
+module "subnet_addrs" {
+  source = "hashicorp/subnets/cidr"
+
+  base_cidr_block = var.base_cidr_block
+  networks = [
+    {
+      name     = "private-eu-central-1a"
+      new_bits = 2
+    },
+    {
+      name     = "private-eu-central-1b"
+      new_bits = 2
+    },
+    {
+      name     = "private-eu-central-1c"
+      new_bits = 2
+    },
+    {
+      name     = "public-eu-central-1a"
+      new_bits = 8
+    },
+    {
+      name     = "public-eu-central-1b"
+      new_bits = 8
+    },
+    {
+      name     = "public-eu-central-1c"
+      new_bits = 8
+    },
+    {
+      name     = "database-eu-central-1a"
+      new_bits = 8
+    },
+    {
+      name     = "database-eu-central-1b"
+      new_bits = 8
+    },
+    {
+      name     = "database-eu-central-1c"
+      new_bits = 8
+    }
+  ]
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "vega-course-tf-vpc"
   cidr = module.subnet_addrs.base_cidr_block
 
-  azs = var.availability_zones
+  azs = data.aws_availability_zones.available.names
 
   public_subnets = [
     lookup(module.subnet_addrs.network_cidr_blocks, "public-eu-central-1a", "what?"),
@@ -28,13 +76,5 @@ module "vpc" {
 
   #internet gateway
   create_igw = true
-
-
-  tags = {
-    # Name = "vega-course-tf-vpc"
-  }
 }
 
-output "nat_gateway_ids" {
-  value = module.vpc.private_nat_gateway_route_ids
-}
--- infrastrucutre/variables.tf
+++ infrastrucutre/variables.tf
@@ -3,9 +3,3 @@ variable "base_cidr_block" {
   type        = string
   default     = "172.16.0.0/16"
 }
-
-variable "availability_zones" {
-  description = "List of Availability Zones"
-  type        = list(string)
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-}
