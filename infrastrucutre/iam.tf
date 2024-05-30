2188e31ffdd5d8e2595b2f964272b30ec7cfa13a
Stefan Aradjanin <stefan.aradjanin@vegait.rs>
2024-05-30 09:53:24 +0200

    Changed alb namespace

--- infrastrucutre/iam.tf
+++ infrastrucutre/iam.tf
@@ -80,7 +80,7 @@ module "iam_irsa-alb-controller" {
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["default:alb-controller"]
+      namespace_service_accounts = ["alb-namespace:alb-controller"]
     }
   }
 
