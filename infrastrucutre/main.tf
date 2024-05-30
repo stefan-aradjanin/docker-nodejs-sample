ec3fddad73f85f3801032057695415bff5b54d67
Stefan Aradjanin <stefan.aradjanin@vegait.rs>
2024-05-30 11:39:15 +0200

    Added Helm provider

--- infrastrucutre/main.tf
+++ infrastrucutre/main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.13.2"
+    }
   }
 }
 
@@ -13,6 +18,18 @@ provider "aws" {
   default_tags {
     tags = {
       Owner = "Stefan Arađanin"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
     }
   }
 }
